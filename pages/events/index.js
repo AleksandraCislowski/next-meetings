44cfd5cadf76d80655ee3ecdab4dd590c4a8bd26
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-20 22:00:29 +0100

    slug fixed

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,19 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 function AllEvents() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <div>
-      <EventSearch />
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </div>
   );
