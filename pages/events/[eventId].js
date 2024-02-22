c30e10ce00fa2ee2f5e929f21f87bd5bef22df90
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-22 16:05:41 +0100

    hotfix

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,4 @@
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
@@ -39,11 +39,11 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 }
 
