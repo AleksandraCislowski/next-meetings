87354431e8239c532cb1ddfd4486e27967c4ee83
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-27 18:10:22 +0100

    context in layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,25 @@
 import MainHeader from "./main-header";
-import { Fragment } from "react";
+import Notification from "../ui/notification";
+import { Fragment, useContext } from "react";
+import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
       <main>
         <MainHeader />
         {props.children}
       </main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </Fragment>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import Head from "next/head";
-import Notification from "../components/ui/notification";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-      <Notification title='test' message='wohooo test' status='pending' />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
