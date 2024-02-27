1070dad1a0aee71e0cd8fc25aef8c1bbb9c67c4c
aleksandra <aleksandra.swigon91@gmail.com>
2024-02-27 17:47:18 +0100

    notofication store

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Notification from "../components/ui/notification";
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
+      <Notification title='test' message='wohooo test' status='pending' />
     </Layout>
   );
 }
